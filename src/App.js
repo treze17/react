a06c360c822ee580d3e9cee3ff799090441442c9
wedia <wediaishaq17@gmail.com>
2023-04-03 23:13:22 +0000

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 
-import './App.css';
+import './index.css';
 import Employee from './components/employee'
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 function App() {
   const [role, setRole] = useState('dev');
-  console.log("We are displaying employee list")
+  const [employees, setEmployees] = useState(
+    [
+      {
+      name : 'King',
+      role : 'Coder',
+      img : 'https://images.pexels.com/photos/5727885/pexels-photo-5727885.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name : 'Wedia',
+      role : 'jnr Dev',
+      img : 'https://images.pexels.com/photos/5727885/pexels-photo-5727885.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name : 'Saud',
+      role : 'Senior Dev',
+      img : 'https://images.pexels.com/photos/5727885/pexels-photo-5727885.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      name : 'Saud',
+      role : 'Senior Dev',
+      img : 'https://images.pexels.com/photos/5727885/pexels-photo-5727885.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }
+   
+    ]
+  );
+  
   const showEmployees = true;
   return (
     <div className="App">
       
 {showEmployees ? (
   <>
-  <input type='text' onChange = {(e) => {
-    console.log(e.target.value)
-    setRole(e.target.value)
-  }}/>
-  <Employee name = "Wedia" role = "Intern"/>
-  <Employee name = "Saud" role = {role}/>
+ 
+  <div className='flex flex-wrap justify-center'>
+
+ {employees.map((ele)=>{
+  console.log(ele);
+  return(
+       <Employee
+       key ={uuidv4()}
+          name={ele.name} 
+          role={ele.role} 
+          img={ele.img}
+        />
+      );
+ })}
+
+
+ </div>
   </>
 ): (
   <p>You can't see the list!!!</p>
 )}
     </div>
   );
+  
 }
 
 export default App;
